3a1baf50712e56bd91e101f7ee2b2ad2ae95af1d
Scott Kirkland <srkirkland@gmail.com>
2019-12-30 11:42:04 -0800

    people class

--- src/FacultyDirectory.Core/Migrations/20191230193834_CreatePeople.Designer.cs
+++ src/FacultyDirectory.Core/Migrations/20191230193834_CreatePeople.Designer.cs
@@ -2,14 +2,16 @@
 using FacultyDirectory.Core.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FacultyDirectory.Core.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191230193834_CreatePeople")]
+    partial class CreatePeople
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
