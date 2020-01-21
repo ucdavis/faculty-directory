84915b099f08aebaa51b3cea5ea83857d2bfdbb4
Unknown <srkirkland@ucdavis.edu>
2020-01-21 13:52:50 -0800

    add sass

--- src/FacultyDirectory/ClientApp/src/App.tsx
+++ src/FacultyDirectory/ClientApp/src/App.tsx
@@ -4,7 +4,7 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { People } from './components/People';
 
-import './custom.css'
+import './custom.scss'
 import { Person } from './components/Person';
 
 export default class App extends Component {
