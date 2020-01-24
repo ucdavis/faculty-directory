243fba2757b637019a3e8bde71a3eab3491fc6b8
Unknown <srkirkland@ucdavis.edu>
2020-01-24 10:34:58 -0800

    add two new code projects

---
+++ src/FacultyDirectory.Jobs.Core/JobBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FacultyDirectory.Jobs.Core
+{
+    public abstract class JobBase
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        protected static void Configure()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (string.Equals(environmentName, "development", StringComparison.OrdinalIgnoreCase))
+            {
+                builder.AddUserSecrets<JobBase>();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            // TODO: setup logs
+            // LogConfiguration.Setup(Configuration);
+        }
+    }
+}
---
+++ src/FacultyDirectory.Jobs.ImportFaculty/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FacultyDirectory.Jobs.ImportFaculty
+{
+    public class Program : JobBase
+    {
+        static void Main(string[] args)
+        {
+            // base config
+            Configure();
+
+            var assembyName = typeof(Program).Assembly.GetName();
+            _log = Log.Logger
+                .ForContext("jobname", assembyName.Name)
+                .ForContext("jobid", Guid.NewGuid());
+
+            _log.Information("Running {job} build {build}", assembyName.Name, assembyName.Version);
+
+            // setup di
+            // TODO
+
+            _log.Information("Import Faculty Job Finished");
+        }
+    }
+}
