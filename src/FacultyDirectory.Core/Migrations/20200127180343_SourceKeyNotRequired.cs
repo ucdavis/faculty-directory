cdffb437e0b956e322f0c8e17aa8402e53030d92
Unknown <srkirkland@ucdavis.edu>
2020-01-27 10:06:27 -0800

    source key no longer required

---
+++ src/FacultyDirectory.Core/Migrations/20200127180343_SourceKeyNotRequired.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FacultyDirectory.Core.Migrations
+{
+    public partial class SourceKeyNotRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SourceKey",
+                table: "PeopleSources",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SourceKey",
+                table: "PeopleSources",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128,
+                oldNullable: true);
+        }
+    }
+}
