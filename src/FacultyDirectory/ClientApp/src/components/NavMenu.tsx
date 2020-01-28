b10067ce78e1f573dae5df5d42b94864a27bb534
Unknown <srkirkland@ucdavis.edu>
2020-01-27 17:15:13 -0800

    fix warnings

--- src/FacultyDirectory/ClientApp/src/components/NavMenu.tsx
+++ src/FacultyDirectory/ClientApp/src/components/NavMenu.tsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
 interface State {
