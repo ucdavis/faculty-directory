7660c34fbdd80b6f790644bf9078b7e5a0f63f23
Unknown <srkirkland@ucdavis.edu>
2020-01-29 10:52:30 -0800

    add tags to siteperson

---
+++ src/FacultyDirectory.Core/Migrations/20200129184827_SitePersonTags.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FacultyDirectory.Core.Migrations
+{
+    public partial class SitePersonTags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "SitePeople",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "SitePeople");
+        }
+    }
+}
