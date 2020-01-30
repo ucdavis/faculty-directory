85db808797de9d316a4644dc550d556f30e21815
cydoval <cydoval@mac.com>
2020-01-30 14:10:40 -0800

    for kaelan!

--- src/FacultyDirectory/ClientApp/src/components/Home.tsx
+++ src/FacultyDirectory/ClientApp/src/components/Home.tsx
@@ -20,6 +20,62 @@ export class Home extends Component {
           <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
         </ul>
         <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+
+
+      <form>
+        <div className="form-group">
+          <label>Example label</label>
+          <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input placeholder"/>
+        </div>
+        <div className="form-group">
+          <label>Another label</label>
+          <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder"/>
+        </div>
+        <div className="form-group">
+         <label>Example textarea</label>
+         <textarea className="form-control" id="exampleFormControlTextarea1" rows={3}></textarea>
+       </div>
+      </form>
+      <form>
+        <div className="form-group">
+          <label>Email address</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+        </div>
+        <div className="form-group">
+          <label>Example select</label>
+          <select className="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Example multiple select</label>
+          <select multiple className="form-control" id="exampleFormControlSelect2">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Example textarea</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows={3}></textarea>
+        </div>
+      </form>
+      <select className="form-control form-control-lg">
+        <option>Large select</option>
+      </select>
+      <select className="form-control">
+        <option>Default select</option>
+      </select>
+      <select className="form-control form-control-sm">
+        <option>Small select</option>
+      </select>
+      <input className="form-control" type="text" placeholder="Readonly input here..." readOnly/>
       </div>
     );
   }
