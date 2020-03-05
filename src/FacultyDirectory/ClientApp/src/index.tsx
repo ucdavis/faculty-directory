a3e1bf3fc9bfccc0e581fe1ef82b5ca201f34dba
Unknown <srkirkland@ucdavis.edu>
2020-03-05 15:30:44 -0800

    playing around with logrocket

--- src/FacultyDirectory/ClientApp/src/index.tsx
+++ src/FacultyDirectory/ClientApp/src/index.tsx
@@ -2,12 +2,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import LogRocket from 'logrocket';
+
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
 import { unregister } from './registerServiceWorker';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faArrowLeft, faTimes, faPlus, fas } from '@fortawesome/free-solid-svg-icons';
+
+LogRocket.init('hlx0h5/facultydirectory');
 
 library.add(faArrowLeft, faTimes, faPlus, fas)
 
@@ -20,5 +23,4 @@ ReactDOM.render(
   </BrowserRouter>,
   rootElement);
 
-// registerServiceWorker();
 unregister(); // remove service workers
