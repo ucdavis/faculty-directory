4f7508cbb5488500a0869e02f32b303797ce4b00
Scott Kirkland <srkirkland@gmail.com>
2022-06-28 10:50:58 -0700

    shell of a pronounce page

---
+++ src/FacultyDirectory/ClientApp/src/components/Pronunciation.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+import { useParams } from 'react-router-dom';
+import { IBio } from '../models/IBio';
+
+import { ISitePerson } from '../models/ISitePerson';
+import { Loading } from './Loading';
+
+export const Pronunciation = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const [bio, setBio] = useState<IBio>();
+  const [sitePerson, setSitePerson] = useState<ISitePerson>({} as ISitePerson);
+
+  useEffect(() => {
+    const fetchPerson = async () => {
+      const result = await fetch('api/sitepeople/' + id).then(r => r.json());
+
+      setBio(result.bio);
+      setSitePerson(result.sitePerson || {});
+    };
+
+    fetchPerson();
+  }, [id]);
+
+  if (!bio || !sitePerson.person) {
+    return <Loading text='LOADING...'></Loading>;
+  }
+
+  return (
+    <div>
+      <h1>Pronunciation for {bio.firstName} {bio.lastName}</h1>
+    </div>
+  );
+};
