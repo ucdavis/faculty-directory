0c49b88b5873c1a6b14fc97a49ca51d24a61a5cd
Scott Kirkland <srkirkland@gmail.com>
2022-08-02 14:15:59 -0700

    migration to add new pronunciation col

---
+++ src/FacultyDirectory.Core/Migrations/20220802211236_Pronunciation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FacultyDirectory.Core.Migrations
+{
+    public partial class Pronunciation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PronunciationUid",
+                table: "SitePeople",
+                type: "uniqueidentifier",
+                maxLength: 64,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PronunciationUid",
+                table: "SitePeople");
+        }
+    }
+}
