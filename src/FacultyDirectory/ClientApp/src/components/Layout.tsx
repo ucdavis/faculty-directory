102a1536e74281b40b36944050e62b0295d7b220
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2026-02-18 15:49:55 -0800

    Don't do absolute path imports from public/

--- src/FacultyDirectory/ClientApp/src/components/Layout.tsx
+++ src/FacultyDirectory/ClientApp/src/components/Layout.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { NavMenu } from './NavMenu';
-import logoUrl from '/media/caes-logo.svg';
 
 export const Layout = () => {
   return (
     <div>
       <div className='caes-wrapper'>
         <a href='https://caes.ucdavis.edu' target='_blank' rel='noreferrer'>
-          <img src={logoUrl} alt='UCDAVIS CAES wordmark' />
+          <img src='/media/caes-logo.svg' alt='UCDAVIS CAES wordmark' />
         </a>
       </div>
       <NavMenu />
