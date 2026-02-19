c01b96864031b7e4a6dc67be81e59db3939b9ed5
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2026-02-19 12:59:10 -0800

    Add defaults on SerilogSettings

--- src/FacultyDirectory.Core/Models/SerilogSettings.cs
+++ src/FacultyDirectory.Core/Models/SerilogSettings.cs
@@ -2,9 +2,9 @@ namespace FacultyDirectory.Core.Models
 {
     public class SerilogSettings
     {
-        public string AppName { get; set; }
-        public string Environment { get; set; }
-        public string OtelEndpoint { get; set; }
-        public string OtelAuthHeader { get; set; }
+        public string AppName { get; set; } = "";
+        public string Environment { get; set; } = "";
+        public string OtelEndpoint { get; set; } = "";
+        public string OtelAuthHeader { get; set; } = "";
     }
 }
