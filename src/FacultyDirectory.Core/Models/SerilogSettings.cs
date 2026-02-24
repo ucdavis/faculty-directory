353acbcbcabb4b2c26310fa2e06ec6a5360b9bd3
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2026-02-24 09:35:55 -0800

    Enable Serilog SelfLog via flag

--- src/FacultyDirectory.Core/Models/SerilogSettings.cs
+++ src/FacultyDirectory.Core/Models/SerilogSettings.cs
@@ -6,5 +6,6 @@ namespace FacultyDirectory.Core.Models
         public string Environment { get; set; } = "";
         public string OtelEndpoint { get; set; } = "";
         public string OtelAuthHeader { get; set; } = "";
+        public bool SelfLog { get; set; } = false;
     }
 }
